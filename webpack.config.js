7b81324f7a5eab2a2420482cae33003927f837a9
Ben <bconrad18@gmail.com>
2018-07-28 21:15:35 -0500

    Initial push for es6 babel webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/public/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+    // output: {
+    //     path: path.resolve("dist"),
+    //     filename: "bundled.js"
+    // },
+    module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use:{
+          loader: "babel-loader"
+        }
+      },{
+        test: /\.css$/,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};

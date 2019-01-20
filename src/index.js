475f6a46408b45ed18b63a6bd78da976325735e6
Ben <bconrad18@gmail.com>
2019-01-19 22:06:44 -0600

    Adding redux store to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './App';
+import {Provider} from 'react-redux';
+import configureStore from './redux/configureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
-    <App/>, document.getElementById("root")
+  <Provider store={store}>
+    <App/>
+  </Provider>, document.getElementById('root'),
 );
